4af861669cbf4a4cb05426dc32ebaf2f763c7acf
Matt Reisdorf Personal <mreisdorf9717@gmail.com>
2025-03-23 20:44:48 -0400

    push to work on laptop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const handleLogin = () => {
+    window.location.href = "http://localhost:8080/login";
+  }
+
+  const playlistUrl = "https://open.spotify.com/playlist/7pkWzBYeI8UpeKMfpGjgKD?si=ae044ff94d3f4fbb"
+  const playlistId = "ae044ff94d3f4fbb"
+  
+  const [userData, setUserData] = useState(null);
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/user", {withCredentials: true})
+      .then(response => {
+        setUserData(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching user data", error);
+      });
+  }, []);
+
+  
+
+  if (userData) {
+    return (
+      <div>
+      {/* {userData ? <p>{JSON.stringify(userData, null, 2)}</p> : <p>Loading user data...</p>}     */}
+      </div>
+    )
+  } else {
+    return (
+      <button onClick = {handleLogin}>Login</button>
+    )
+  }
 }
 
 export default App;
